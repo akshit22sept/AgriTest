51d1ba8c95f41b456a103d3a620ebb580272496d
akshit22sept <64702748+akshit22sept@users.noreply.github.com>
2024-11-11 19:00:39 +0530

    Updated Model

--- Models.py
+++ Models.py
@@ -14,13 +14,19 @@ WheatClasses=["Aphid",'Black_Rust','Blast','Brown_Rust','Common_Root_Rot','Fusar
 AppleClasses=['Apple_scab', 'Black_rot', 'Cedar_apple_rust', 'healthy']
 TomatoClasses=['Bacterial_spot','Early_blight','healthy','Late_blight','Leaf_Mold','Septoria_leaf_spot','Spider_mites Two-spotted_spider_mite','Target_Spot','Tomato_mosaic_virus','Tomato_Yellow_Leaf_Curl_Virus']
 PotatoClasses=["Early Blight","Healthy","Late Blight"]
+SPotato=['Soybean',"Pepper,"]
+SApple=["Cherry","Blueberry"]
+SWheat=["Corn"]
+SSugarcane=["Peach"]
+SMango=['Tea']
+
 AllClasses=[AllLeavesClasses,AppleClasses,PotatoClasses,TomatoClasses,WheatClasses]
 def GetInfo(Name,NameList,ModelList):
     return ModelList[NameList.index(Name)]
 
 ModelName=[]
 Model=[]
-
+m=load_model("AllLeavesTesting.h5")
 for i in os.listdir("Models"):
     ModelName.append(i.split("Model")[0])
     Model.append(load_model(f"Models\\{i}"))
@@ -40,13 +46,24 @@ def Predict(image):
     a = resize(image, [255, 255], anti_aliasing=True)
     a1 = np.expand_dims(a, axis=0)
     predict=GetInfo("AllLeaves",ModelName,Model).predict(a1)
+    predict2=m.predict(a1)
 
-
-    classes_x=np.argmax(predict,axis=1)
+    Pred=predict+predict2
+    classes_x=np.argmax(Pred,axis=1)
     LeafType=GetInfo("AllLeaves",ModelName,AllClasses)[int(classes_x)]
     e=predict[0]*100
-    
-    print([i for i in e if i>1])
+    if LeafType in SApple:
+        LeafType = "Apple"
+    if LeafType in SPotato:
+        LeafType = "Potato"
+    if LeafType in SWheat:
+        LeafType = "Wheat"
+    if LeafType in SSugarcane:
+        LeafType = "Sugarcane"
+    if LeafType in SMango:
+        LeafType="Mango"
+
+
     if [i for i in ModelName if LeafType in ModelName]:
         predictDis=GetInfo(LeafType,ModelName,Model).predict(a1)
         ClassDis = np.argmax(predictDis, axis=1)
