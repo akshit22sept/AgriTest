98b7d9189dfcf2c521855b9329fc3e1500a5288b
Chaitanya Agaj <chaitanya.agaj63@gmail.com>
2024-12-02 19:30:50 +0530

    Changing file name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Hello_world():
-    return render_template('index.html')
+    return render_template('layout_1.html')
     # return "<p>Hello World!</p>"
 
 
